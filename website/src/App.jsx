4a61ad05e8a0aa1b5613460dbc7c7ee50294c473
ndree2005 <andrendraha@gmail.com>
2026-05-01 00:04:12 +0700

    bg-gradient

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -9,7 +9,7 @@ import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div className="relative h-full w-full text-white">
-      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-size-[20px_20px]"></div>
       <Routes className="">
         <Route path="/" element={<HomePages />} />
         <Route path="/course" element={<CoursePage />} />
