9817df4812404d2418354b0e070eda225a861ee2
ndree2005 <andrendraha@gmail.com>
2026-05-01 16:29:05 +0700

    i crreated theree file new

---
+++ mobile/app/page/page.tsx
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const page = () => {
+  return (
+    <View>
+      <Text>Hello, React Native!</Text>
+    </View>
+  )
+}
+
+export default page
